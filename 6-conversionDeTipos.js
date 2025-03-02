ea605db4b03748fee063495dc0250f9bffdfa085
LeoGNavaC <leonava988@gmail.com>
2025-03-02 15:22:20 -0600

    Agregando nueva linea de comentario

--- 6-conversionDeTipos.js
+++ 6-conversionDeTipos.js
@@ -5,6 +5,6 @@ const edadUsuario   = "39";
 console.log("Aqui nos junta los numero pensando que son cadenas", edad + edadUsuario);//Esto es implicito tomo los dos datos como cadenas de caracteres y me sale 3939
 
 //Conversion explicita
-//Siempre hay qye usar conversion explicita y no dejar a java que asuma la variable
+//Siempre hay qye usar conversion explicita y no dejar a java que asuma la variable 
 console.log("Aqui nos lo suma ya que convertimos a numero la cadena", edad + Number(edadUsuario));//Aqui si lo suma, debido a que lo comvierte 
 console.log("Aqui si es explicito, le indicamos",String(edad) + edadUsuario);
