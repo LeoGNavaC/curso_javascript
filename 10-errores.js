11c5613aeca21a66fce5d26bfbf6d661dae82f0d
LeoGNavaC <leonava988@gmail.com>
2025-03-08 14:28:09 -0600

    Agregando manejo de errores con console.log

---
+++ 10-errores.js
@@ -0,0 +1,7 @@
+const edad  = 39;
+
+console.log(edadPersona); //error de variable indefinida
+
+const Edad;
+
+console.log(Edad);//Error de sintaxis
---
+++ 11-console.api.js
@@ -0,0 +1,17 @@
+//inicio del conteo 
+console.time('Tiempo');
+
+//mostrando mensajes
+console.log("Hola mundoooo");
+
+const edad  = 40;
+console.log(edad);
+
+//Mostrando un error
+console.error("variable debe ser mayor a 10"); //Estos podemos verlos de otro color en la consola
+
+//Mostrando una advertencia
+console.warn("Debe indicar un valor");//Igual se ve con otro color en la consola
+
+//Final del tiempo
+console.timeEnd('Tiempo');
