95251ff503a0a10d09a1583b169b13052852dfcd
LeoGNavaC <leonava988@gmail.com>
2025-03-08 15:09:08 -0600

    Se agregan archivo 13 js donde nos muestra diferente forma de usar l condicion if

---
+++ 13-operadorTernario.js
@@ -0,0 +1,11 @@
+const edadUsuario   = 25;
+const edadPermitida = 20;
+/*
+if (edadUsuario > edadPermitida){
+    console.log('Venta permitida');
+} else {
+    console.log('Prohibida la venta');
+}
+    */
+//Todo esto es el if          //Aqui entra si es verdadero     //Aqui entra si es falso
+(edadUsuario > edadPermitida) ? console.log('Venta permitida') : console.log('Prohibida la venta');
