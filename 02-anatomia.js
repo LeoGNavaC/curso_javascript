ab49bbe605551b172317eb91f16100106baaadb7
LeoGNavaC <leonava988@gmail.com>
2025-03-09 11:50:57 -0600

    Agregando al array lenght y como usarlo

---
+++ 02-anatomia.js
@@ -0,0 +1,5 @@
+const notas = [9,8.5,6,9.5,'aprobado', true];
+
+console.log(notas[(notas.length-notas.length)]);//nos trae el primer dato
+console.log(notas[notas.length-1])/*con esto le digo que me traiga el ultimo dato del arreglo, 
+                                    el menos uno es debido a que cuenta de 1234 y en el array es 0123*/
