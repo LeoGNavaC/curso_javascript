e3d63c62d0fbbaad316540019d49a3117c40d6b6
LeoGNavaC <leonava988@gmail.com>
2025-04-27 14:15:43 -0600

    Archivo para ordenar dos arrays en uno nuevo

---
+++ algoritmos2/arrays.js
@@ -0,0 +1,52 @@
+//vamos a obtener el precio mas bajo de los que tenemos en el array
+const edtPlaneta  = [
+    {
+        nombre: "Java",
+        precio: 15,
+    },
+    {
+        nombre: "PHP",
+        precio: 20,
+    },
+    {
+        nombre: "Python",
+        precio: 25,
+    },
+    {
+        nombre: "Javascript",
+        precio: 30,
+    },
+    {
+        nombre: "Go",
+        precio: 35,
+    },
+];
+
+const edtSuperTecno  = [
+    {
+        nombre: "Dart",
+        precio: 15,
+    },
+    {
+        nombre: "React",
+        precio: 17,
+    },
+    {
+        nombre: "Ruby",
+        precio: 22,
+    },
+    {
+        nombre: "C++",
+        precio: 25,
+    },
+    {
+        nombre: "C#",
+        precio: 30,
+    },
+    {
+        nombre: "Rust",
+        precio: 32
+    }
+];
+
+module.exports  = {edtPlaneta,edtSuperTecno};
---
+++ algoritmos2/index.js
@@ -0,0 +1,35 @@
+const listaPrecios = require('../algoritmos/data.js');
+const {edtPlaneta, edtSuperTecno}   = require('./arrays.js');
+
+function mezclarListas (lista1,lista2) {
+    let posicionLista1  = 0;
+    let posicionLista2  = 0;
+    let posicionActual  = 0;
+    const listaGeneral  = [];
+
+    while (posicionLista1 < lista1.length && posicionLista2 < lista2.length) {
+        if (lista1[posicionLista1].precio <= lista2[posicionLista2].precio ) {
+            listaGeneral[posicionActual]    = lista1[posicionLista1];
+            posicionLista1++;
+        } else {
+            listaGeneral[posicionActual]    = lista2[posicionLista2];
+            posicionLista2++;
+        }
+        posicionActual++;
+    }
+//Con estos whiles comparamos si queda algo dentro de las listas lo agrega a la lista general
+    while (posicionLista1 < lista1.length) {
+        listaGeneral[posicionActual]    = lista1[posicionLista1];
+        posicionLista1++;
+        posicionActual++;
+    }
+
+    while (posicionLista2 < lista2.length) {
+        listaGeneral[posicionActual]    = lista1[posicionLista2];
+        posicionLista2++;
+        posicionActual++;
+    }
+    console.log(listaGeneral);
+}
+
+mezclarListas(edtPlaneta,edtSuperTecno);
