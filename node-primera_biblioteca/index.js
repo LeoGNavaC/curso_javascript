92bd520c8d3ac93639981b774b45c10cae1409a2
LeoNava <leonava988@gmail.com>
2025-05-28 20:32:32 -0600

    Agregando libreria chalk para colocar color en la linea de comandos

---
+++ node-primera_biblioteca/index.js
@@ -0,0 +1,21 @@
+import chalk from 'chalk';
+
+console.log(chalk.red('Hello world!'));
+
+console.log("Hola mundo");
+
+console.log(`La interfas File nos proporciona informacion relacionada a archivos
+que permite a JavaScript poder acceder a su contenido.
+
+Son generalmente generados a partir de un objeto [FileList](https://
+developer.mozilla.org/en-US/docs/Web/API/FileList), el cual es el
+resultado de la seleccion del archivo dado por el a traves el
+elemento input[<input>](https://developer.mozilla.org/en-US/docs/
+Web/HTML/Element/Input), a partir del objeto [DataTransfer](https://
+developer.mozilla.org/en-US/docs/Web/API/DataTransfer) utilizado en
+operaciones Drag and Drop o en español Arrastrar y Soltar o a partir 
+de la API mozGetAsFile() en un [HTMLCanvasElement](https://
+developer.mozilla.org/en-US/docs/Web/API/Canvas_API).
+
+[Prueba code 400](https://httpstat.us/404)
+[Gato salchicha](http://gatosalchica.com.mx/)`);
