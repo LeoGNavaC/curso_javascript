06c55d672809c9c124c41cabc2e9857fb8c28a9f
LeoNava <leonava988@gmail.com>
2025-06-16 21:58:38 -0600

    Agregando Object.setPrototypeof, para heredar metodos de los objetos

---
+++ POOJS/objetoLiteral.js
@@ -0,0 +1,29 @@
+const usuario   = {
+    nombre: "Mariana",
+    email: "m@m.com",
+    nacimiento: "11/10/1989",
+    rol: "estudiante",
+    activo: true,
+    mostrarDatos: function(){
+        console.log(this.nombre,this.email);
+    }
+};
+
+const admin = {
+    nombre: "Leonardo",
+    email: "leonava988@gmail.com",
+    nacimiento: "29/08/2001",
+    rol: "admin",
+    activo: true,
+    crearCurso: function(){
+        console.log("Curso Creado¡¡");
+    }
+}
+
+Object.setPrototypeOf(admin,usuario);//con esto le indico que puedo traer un protpotipo de los metodos que tiene el otro objeto
+
+admin.__proto__;
+
+admin.crearCurso();
+admin.mostrarDatos();
+//Herencia de prototipos
